a49eb6a2655fcf4657d053a0e38a6bd90b66f940
chazzyjazzy <33268885+chazzyjazzy@users.noreply.github.com>
2026-01-07 21:31:17 -0500

    jobz

--- tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferences/JobsPage.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferences/JobsPage.tsx
@@ -378,8 +378,8 @@ export function JobsPage() {
                 <Department department="Manor" />
                 <Department department="Sabbat" />
                 <Department department="Citizen" />
-                <Department department="City Services" />
-                <Department department="Garou Nation" />
+                <Department department="City_Services" />
+                <Department department="Garou_Nation" />
               </Stack>
             </Stack.Item>
             {
