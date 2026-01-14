c5d6373ecdfcb31ae7a70246a40f1766a4d9c33d
XeonMations <62395746+XeonMations@users.noreply.github.com>
2026-01-14 17:43:47 +0200

    SQL shenanigens

--- SQL/tgstation_schema.sql
+++ SQL/tgstation_schema.sql
@@ -261,7 +261,7 @@ CREATE TABLE `library` (
   `author` varchar(45) NOT NULL,
   `title` varchar(45) NOT NULL,
   `content` text NOT NULL,
-  `category` enum('Any','Fiction','Non-Fiction','Adult','Reference','Religion') NOT NULL,
+  `category` enum('Any','Fiction','Non-Fiction','Adult','Reference','Religion','Kindred','Lupine','Kuei-Jin') NOT NULL, /* DARKPACK EDIT, ORIGINAL: `category` enum('Any','Fiction','Non-Fiction','Adult','Reference','Religion') NOT NULL, */
   `ckey` varchar(32) NOT NULL DEFAULT 'LEGACY',
   `datetime` datetime NOT NULL,
   `deleted` tinyint(1) unsigned DEFAULT NULL,
