52a5d21e1322ce6411041a1dc75d307df33d0e5b
FalloutFalcon <falloutfalconplays@gmail.com>
2026-03-23 13:03:00 -0500

    renown stop gap

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/darkpack_renown.tsx
@@ -0,0 +1,20 @@
+// THIS IS A DARKPACK UI FILE
+import { FeatureNumberInput, type FeatureNumeric } from '../base';
+
+export const honor: FeatureNumeric = {
+  name: 'Honor',
+  description: 'The Honor of this fera.',
+  component: FeatureNumberInput,
+};
+
+export const glory: FeatureNumeric = {
+  name: 'Glory',
+  description: 'The Glory of this fera.',
+  component: FeatureNumberInput,
+};
+
+export const wisdom: FeatureNumeric = {
+  name: 'Wisodm',
+  description: 'The Wisdom of this fera.',
+  component: FeatureNumberInput,
+};
