59f014757c8137e56fdd268103a1726ae514822d
FalloutFalcon <falloutfalconplays@gmail.com>
2026-04-13 22:58:30 -0500

    yea

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/darkpack_quirks.tsx
+++
@@ -1,7 +0,0 @@
-import type { FeatureChoiced } from '../base';
-import { FeatureDropdownInput } from '../dropdowns';
-
-export const missing_arm: FeatureChoiced = {
-  name: 'Missing Arm',
-  component: FeatureDropdownInput,
-};
