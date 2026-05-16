25b98e635a6142140265ea394c1b5fa6d572b69f
Kolibri-B <physicalduck1@gmail.com>
2026-05-16 06:34:06 -0500

    IrisStation Fancy Paperwork Port

--- tgui/packages/tgui/interfaces/ImplantPad.tsx
+++ tgui/packages/tgui/interfaces/ImplantPad.tsx
@@ -1,3 +1,4 @@
+import { sanitizeText } from 'tgui/sanitize';
 import {
   Button,
   Collapsible,
@@ -29,6 +30,13 @@ export const ImplantPad = (props) => {
     saved_deathrattle_group,
     current_deathrattle_group,
   } = data;
+  const sanitized = sanitizeText(case_information);
+  const textHtml = {
+    __html:
+      typeof sanitized === 'object' && sanitized !== null
+        ? sanitized.sanitized
+        : sanitized,
+  };
   return (
     <Window width={300} height={350}>
       <Window.Content scrollable>
