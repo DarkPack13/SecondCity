60a9ff3373f3554ed1435f10c5211d3ad47d6903
Hardly3D <66234359+Hardly3D@users.noreply.github.com>
2026-07-23 22:10:37 -0300

    clarity(2)

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/darkpack_masked_flavor_text.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/darkpack_masked_flavor_text.tsx
@@ -3,6 +3,6 @@ import { CheckboxInput, type FeatureToggle } from '../base';
 export const show_flavor_text_when_masked: FeatureToggle = {
   name: 'Show Identity When Masked',
   description:
-    'Show your name to those who know you and flavor text while your face is hidden.',
+    'While toggled and masked, everyone will be able to see your flavor text and your name if they know it.',
   component: CheckboxInput,
 };
